a4caf010c58869135b36a498eadcee9fcb89ca0f
ehab magdob <ehabmj1212@gmail.com>
2025-12-02 13:36:52 +0200

    fix

---
+++ JS/Explain/ex1/Tasks/script.js
@@ -0,0 +1,26 @@
+const first = "ehab";
+const last = "majdob";
+let Birthyear = 1998;
+const Employment = true;
+let salary;
+const bestFriend = null;
+const CurrentYear = 2025;
+
+const age = CurrentYear - Birthyear;
+const fullName = first + " " + last;
+
+console.log(CurrentYear - Birthyear);
+console.log(first + " " + last);
+
+console.log(fullName);
+console.log(typeof fullName);
+console.log(age);
+console.log(typeof age);
+console.log(Employment);
+console.log(typeof Employment);
+console.log(salary);
+console.log(typeof salary);
+console.log(bestFriend);
+console.log(typeof bestFriend);
+console.log(`My name is ${fullName} and i'm a ${age}`);
+
---
+++ JS/Explain/ex1/script.js
@@ -0,0 +1,32 @@
+// Keywords:
+// let
+// var
+// const
+
+// var name = "Mohamed";
+// let name = "ehab";
+
+// not allowed to change
+// const user = "Coco";
+// const user = "Lolo";
+
+const user = " ";
+console.log(user);
+
+// Datatype:
+// type: null, Boolean, String, Double = 23, 555.., int = رقم صحيح, Float = 23, 4, undefined
+
+let age = "23"
+// newAge = age + 5;
+newAge = Number(age) + 5;
+console.log(age);
+
+// let newAge = 0; // number
+// newAge = age + 5; // reassign // we make the newAge as new one
+
+// === بتقارن الفاليو ولداتا تايب
+// == بتقارن بس الفاليو
+
+let a = 5;
+let b = "7";
+console.log(++b + a);
